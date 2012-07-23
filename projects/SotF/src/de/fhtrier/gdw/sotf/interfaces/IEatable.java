8217ebacdabd21c503a02ca7d8baea88e95f1ab3
Stefan Probst <probststefan@gmail.com>
2012-07-23 16:58:40 +0200

    Interfaces IPowerups, IUseable und IEatable angelegt.

---
+++ projects/SotF/src/de/fhtrier/gdw/sotf/interfaces/IEatable.java
@@ -0,0 +1,38 @@
+package de.fhtrier.gdw.sotf.interfaces;
+
+import java.util.List;
+
+/**
+ * Interface zur Verwaltung eines Eatable.
+ * 
+ * @author Kevin Korte
+ * @author Attila Djerdj
+ * @author Stefan Probst
+ */
+public interface IEatable {
+
+	/**
+	 * Liefert eine Liste vom Typ IPowerups
+	 * mit den Powerups die auf den Spieler wirken,
+	 * wenn er das Item isst.
+	 * 
+	 * @return List<IPowerups>
+	 */
+	public List<IPowerups> getPowerups();
+
+	/**
+	 * Liefert das Useable zurück falls
+	 * das Eatable ein Useable enthält.
+	 * 
+	 * @return IUseable
+	 */
+	public IUseable getUseable();
+
+	/**
+	 * Liefert zurück um wieviel der
+	 * Spieler wächst.
+	 * 
+	 * @return float 	 
+	 */
+	public float getEnergy();
+}
