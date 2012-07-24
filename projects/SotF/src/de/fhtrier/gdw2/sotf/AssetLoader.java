50c222601bc87a77c4340f948d2f4943fbe79694
rftpool23 <rftpool23@rft2309.rz.fh-trier.de>
2012-07-24 12:52:31 +0200

    player animation läuft

--- projects/SotF/src/de/fhtrier/gdw2/sotf/AssetLoader.java
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/AssetLoader.java
@@ -7,6 +7,7 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tiled.TiledMap;
 
 import de.fhtrier.gdw2.sotf.Interfaces.ITeam;
@@ -17,15 +18,26 @@ public class AssetLoader {
 	ArrayList<Animation> animations = new ArrayList<Animation>();
 	HashMap<Integer, String> maps = new HashMap<Integer, String>(10);
 	TiledMap map;
+	Animation playerAnimation;
+	SpriteSheet playerSheet;
 	
-	public AssetLoader() {
+	public AssetLoader() throws SlickException {
 		initializeAssets();
 		
 	}
 	
-	private void initializeAssets(){
+	private void initializeAssets() throws SlickException{
+		//Maps anlegen
 		maps.put(0, "Testmap.tmx");
-	}
+		playerSheet = new SpriteSheet("res/animations/character_top.png", 130, 120);
+		playerAnimation = new Animation();
+		playerAnimation.setAutoUpdate(true);
+		for(int col=0 ; col<15 ; ++col){
+			playerAnimation.addFrame(playerSheet.getSprite(col, 0), 80);
+		}
+		//Image sideView = new Image("res/animations/character_side.png");
+		//playerAnimation.addFrame(sideView, 300);
+		}
 	
 	public Sound getCollideSound(){
 		return null;
@@ -41,11 +53,11 @@ public class AssetLoader {
 	 * @param  stage - der index des levels
 	 */
 	public TiledMap loadMap(int stage) throws SlickException{
-		map = new TiledMap("res/" + maps.get(stage) , "res/");
+		map = new TiledMap("res/maps/" + maps.get(stage) , "res/maps/");
 		return map;
 	}
 	
-	public Image getCharacter(ITeam team){
-		return null;
+	public Animation getAnimation(ITeam team){
+		return playerAnimation;
 	}
 }
