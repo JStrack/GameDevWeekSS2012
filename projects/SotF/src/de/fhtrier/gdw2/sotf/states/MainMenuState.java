f17c016369758c0bbbe489deda7bb64f4ffa936a
JStrack <johannesstrack@live.de>
2012-07-24 11:07:11 +0200

    MainMenu State Klasse aktualisiert

--- projects/SotF/src/de/fhtrier/gdw2/sotf/states/MainMenuState.java
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/states/MainMenuState.java
@@ -1,6 +1,11 @@
 package de.fhtrier.gdw2.sotf.states;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.newdawn.slick.AngelCodeFont;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
@@ -8,70 +13,99 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
-/**
- * @author Kevin Korte
- * @author Attila Djerdj
- * @author Stefan Probst
- *
- */
+//import de.fhtrier.gdw2.sotf.Interfaces.IActionListener;
+import de.fhtrier.gdw2.sotf.menu.Button;
 
-public class MainMenuState extends BasicGameState
-{
+public class MainMenuState extends BasicGameState {
 
     int stateID = -1;
 
-    public MainMenuState(int stateID)
-    {
+    public int mouseX, mouseY;
+    public boolean mouseDown;
+    public List<Button> buttons = new ArrayList<Button>();
+    private Font font;
+
+    public MainMenuState(int stateID) {
         this.stateID = stateID;
     }
 
     @Override
-    /**
-     * @param GameContainer
-     * @param StateBasedGame
-     */
-    public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException
-    {
-        // TODO Auto-generated method stub
+    public void init(GameContainer container, StateBasedGame game)
+            throws SlickException {
+        font = new AngelCodeFont("res/demo2.fnt","res/demo2_00.tga");
 
+        float x = container.getWidth()*0.5f;
+        float y = container.getHeight()*0.5f;
+        float h = font.getLineHeight() * 1.2f;
+        addCenteredButton("Create a Game", x, y - h * 2);
+        addCenteredButton("Join a Game", x, y - h * 1);
+        addCenteredButton("Options", x, y);
+        addCenteredButton("Help", x, y + h * 1);
+        addCenteredButton("Exit", x, y + h * 2);
+        for(Button b: buttons)
+            b.init(container, this);
     }
 
     @Override
-    /**
-     * @param GameContainer
-     * @param StateBasedGame
-     * @param Graphics
-     */
-    public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2) throws SlickException
-    {
-        // TODO Auto-generated method stub
-        arg2.setBackground(Color.green);
+    public void render(GameContainer container, StateBasedGame game, Graphics g)
+            throws SlickException {
+        g.clear();
+        for(Button b: buttons)
+            b.render(g);
     }
 
     @Override
-    /**
-     * @param GameContainer
-     * @param StateBasedGame
-     * @param int
-     */
-    public void update(GameContainer arg0, StateBasedGame arg1, int arg2) throws SlickException
-    {
+    public void update(GameContainer container, StateBasedGame game, int delta)
+            throws SlickException {
         // TODO Auto-generated method stub
 
-        if (arg0.getInput().isKeyDown(Input.KEY_1))
-        {
-            arg1.enterState(1);
+        if (container.getInput().isKeyDown(Input.KEY_1)) {
+            game.enterState(1);
         }
 
     }
 
     @Override
-    /**
-     * @return stateID
-     */
-    public int getID()
-    {
+    public int getID() {
         // TODO Auto-generated method stub
         return stateID;
+    }
+
+
+    public void addCenteredButton(final String text, float x, float y) {
+        float w = font.getWidth(text);
+        float h = font.getHeight(text);
+
+        Button button = Button.create(text, x - w/2, y - h/2, w, h)
+            .font(font)
+            .color(Color.gray)
+            .hoverColor(Color.white)
+            .pressedColor(Color.red)
+            .action(
+                new IActionListener() {
+                    public void onAction() {
+                        System.out.println("Button '" + text + "' pressed");
+                    }
+                }
+            );
+        buttons.add(button);
+    }
+
+    public void keyReleased(int key, char c) {
+        if (key == Input.KEY_ESCAPE) {
+        }
+    }
+
+    public void mouseMoved(int oldx, int oldy, int newx, int newy) {
+        mouseX = newx;
+        mouseY = newy;
+    }
+
+    public void mouseReleased(int button, int x, int y) {
+        mouseDown = false;
+    }
+
+    public void mousePressed(int button, int x, int y) {
+        mouseDown = true;
     }
 }
