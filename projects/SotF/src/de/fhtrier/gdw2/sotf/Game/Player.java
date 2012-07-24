6438feff2a1becc6177031eba9386ce1ef7f9cef
rftpool23 <rftpool23@rft2303.rz.fh-trier.de>
2012-07-24 14:28:39 +0200

    Änderung an Klasse Player

--- projects/SotF/src/de/fhtrier/gdw2/sotf/Game/Player.java
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/Game/Player.java
@@ -1,178 +1,168 @@
 package de.fhtrier.gdw2.sotf.Game;
 
+/**
+ * @author Ben Raus
+ * @author Wendelin Lehr
+ * @author Susanne Kühn
+ */
 import java.util.List;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.geom.Vector2f;
 
 import de.fhtrier.gdw2.sotf.Interfaces.*;
 
-public class Player extends Entity implements IPlayer{
-    
-    private Vector2f velocity;
-    private IUseable[] inventory;
-    private List<IPowerups> powerups;
-    private ITeam team;
+public class Player extends Entity implements IPlayer, IEatable {
 
-    public Player(Vector2f position, float radius, int id, Image image) {
-        super(position, radius, id, image);
-    }
+	private Vector2f velocity;
+	private IUseable[] inventory;
+	private List<IPowerups> powerups;
+	private ITeam team;
+	private Animation animation;
 
-    @Override
-    public void setVelocity(Vector2f velo) {
-        this.velocity = velo;
-    }
+	
+	public Player(Vector2f position, float radius, int id, ITeam team, Animation animation) {
+		super(position, radius, id, null);
+		
+		this.animation = animation;
+		this.team = team;
+		this.inventory = new IUseable[1];
+	}
 
-    @Override
-    public Vector2f getVelocity() {
-        return this.velocity;
-    }
-    
-    /**
-     * Steuerung des Spielers
-     * @author Robin Dick
-     * @param gc Der Slick-GameContainer
-     * @param delta Millisekunden seit dem letzten Update-Schritt
-     */
-    private void controll(GameContainer gc, int delta) {
-        // TODO: Feststellen, ob der Player wirklich der lokale Spieler ist!
-        float changeX = 0f;
-        float changeY = 0f;
-        
-        float deltaMovement = Constants.PLAYER_MOVEMENT_SPEED / (1000f * delta);
-        
-        if (gc.getInput().isKeyDown(Input.KEY_W)) {
-            changeY -= deltaMovement; 
-        }
-        if (gc.getInput().isKeyDown(Input.KEY_S)) {
-            changeY += deltaMovement; 
-        }
-        if (gc.getInput().isKeyDown(Input.KEY_A)) {
-            changeX -= deltaMovement; 
-        }
-        if (gc.getInput().isKeyDown(Input.KEY_D)) {
-            changeX += deltaMovement; 
-        }
-        
-        // TODO: Levelgrenzen einhalten!
-        float x = this.getPosition().getX() + changeX;
-        float y = this.getPosition().getY() + changeY;
-        
-        //System.out.println("X: "+this.getPosition().getX()+" | Y: "+this.getPosition().getY());
-        
-        this.setPosition(x, y);
-    }
-    
-    @Override
-    public void update(GameContainer gameContainer, int time) {
-        // Steuerung
-        controll(gameContainer,time);
-        
-        if (powerups != null) {
-            // Duration der Power-ups anpassen/prüfen
-            for (int i = 0; i <= this.powerups.size(); i++) {
-                this.powerups.get(i).setDuration(
-                        this.powerups.get(i).getDuration() - time);
-    
-                // abgelaufene Powerups entfernen
-                if (this.powerups.get(i).getDuration() <= 0) {
-                    // effekt entfernen
-                    // TODO
-                    // Powerup aus Liste löschen
-                    this.powerups.remove(i);
-                }
-            }
-        }
-    }
-    
-    /**
-     * Liefert zurück, ob der Spieler grade gegessen werden kann oder
-     * immortal-Effekt auf den Spieler zutreffen
-     * 
-     * @return
-     */
-    public boolean isEatable() {
-        for (int i = 0; i < this.powerups.size(); i++) {
-            if (this.powerups.get(i).getType() == IPowerups.PowerupType.IMMORTAL)
-                return false;
-        }
-        return true;
-    }
+	@Override
+	public void setVelocity(Vector2f velo) {
+		this.velocity = velo;
+	}
 
-    @Override
-    public void eat(IEatable eat) {
+	@Override
+	public Vector2f getVelocity() {
+		return this.velocity;
+	}
 
-        // Wachsen des Spielers
-        this.setRadius(this.getRadius() + eat.getEnergy());
+	@Override
+	public void update(GameContainer gameContainetr, int time) {
 
-        // Usable zu Inventar hinzufügen
-        IUseable use = eat.getUseable();
-        if (use != null) {
-            // TODO
-        }
+		// Duration der Power-ups anpassen/prüfen
+		for (int i = 0; i <= this.powerups.size(); i++) {
+			this.powerups.get(i).setDuration(
+					this.powerups.get(i).getDuration() - time);
 
-        // Powerups hinzufügen und Effekt anwenden
-        List<IPowerups> plist = eat.getPowerups();
-        if (plist != null) {
-            for (int i = 0; i < plist.size(); i++) {
-                // Powerup zu Liste hinzufügen
-                this.powerups.add(plist.get(i));
+			// abgelaufene Powerups entfernen
+			if (this.powerups.get(i).getDuration() <= 0) {
+				
+				// Effekt entfernen
+				switch (this.powerups.get(i).getType()) {
+				case SPEED:
+					this.velocity.scale(1/this.powerups.get(i).getValue());	// prozentualer Wert zur Skalierung erwartet
+					break;
+				case IMMORTAL:	// es werden keine Attribute des Players verändert -> Prüfung erolgt in isEatable()-Methode
+					break;
+				case SIGHT:
+					break;
+				}
+				
+				// Powerup aus Liste löschen
+				this.powerups.remove(i);
+			}
+		}
 
-                // Effekt auf Spieler anwenden
-                switch (plist.get(i).getType()) {
-                case SPEED:
-                    break;
-                case IMMORTAL:
-                    break;
-                case SIGHT:
-                    break;
-                }
-            }
-        }
+	}
 
-    }
+	/**
+	 * Liefert zurück, ob der Spieler grade gegessen werden kann oder
+	 * immortal-Effekt auf den Spieler zutreffen
+	 * 
+	 * @return
+	 */
+	public boolean isEatable() {
+		for (int i = 0; i < this.powerups.size(); i++) {
+			if (this.powerups.get(i).getType() == IPowerups.PowerupType.IMMORTAL)
+				return false;
+		}
+		return true;
+	}
 
-    @Override
-    public IUseable use(int number) {
+	@Override
+	public void eat(IEatable eat) {
 
-        if (number >= 0 && number < this.inventory.length)
-            return this.inventory[number];
-        else
-            return null;
-    }
+		// Wachsen des Spielers
+		if(this.getRadius() + eat.getEnergy() > Constants.MAX_SIZE)	
+			this.setRadius(Constants.MAX_SIZE);
+		else
+			this.setRadius(this.getRadius() + eat.getEnergy());
 
-    @Override
-    public IUseable[] getInventory() {
-        return this.inventory;
-    }
+		// Usable zu Inventar hinzufügen
+		IUseable use = eat.getUseable();
+		if (use != null) {
+			//@TODO hhh
+		}
 
-    @Override
-    public List<IPowerups> getPowerups() {
-        return this.powerups;
-    }
+		// Powerups hinzufügen und Effekt anwenden
+		List<IPowerups> plist = eat.getPowerups();
+		if (plist != null) {
+			for (int i = 0; i < plist.size(); i++) {
+				// Powerup zu Liste hinzufügen
+				this.powerups.add(plist.get(i));
 
-    @Override
-    public ITeam getTeam() {
-        return this.team;
-    }
+				// Effekt auf Spieler anwenden
+				switch (plist.get(i).getType()) {
+				case SPEED:
+					this.velocity.scale(plist.get(i).getValue());	// prozentualer Wert zur Skalierung erwartet
+					break;
+				case IMMORTAL:	// es werden keine Attribute des Players verändert -> Prüfung erolgt in isEatable()-Methode
+					break;
+				case SIGHT:  
+					break;
+				}
+			}
+		}
 
-    @Override
-    public boolean isDead() {
-        return this.getRadius() <= 0; // TODO: woher kommt die MinSize?
-    }
-    
-    @Override
-    public IUseable getUseable() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	}
 
-    @Override
-    public float getEnergy() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
+	@Override
+	public IUseable use(int number) {
+
+		if (number >= 0 && number < this.inventory.length)
+			return this.inventory[number];
+		else
+			return null;
+	}
+
+	@Override
+	public IUseable[] getInventory() {
+		return this.inventory;
+	}
+
+	@Override
+	public List<IPowerups> getPowerups() {
+		return this.powerups;
+	}
+
+	@Override
+	public ITeam getTeam() {
+		return this.team;
+	}
+
+	@Override
+	public boolean isDead() {
+		return this.getRadius() < Constants.MIN_SIZE; 
+	}
+
+	@Override
+	public IUseable getUseable() {
+		return null;
+	}
+
+	@Override
+	public float getEnergy() {
+		return this.getRadius();	// Frage an das Game-Design: Faktor?
+	}
+	
+	@Override
+	public void render(Graphics g)
+	{
+		// TODO Animation anzeigen
+	}
 
 }
