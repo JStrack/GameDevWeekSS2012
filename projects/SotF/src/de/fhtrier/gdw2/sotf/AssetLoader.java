f58274605989a67debf74068899d4e4eab82f3b5
Torsten Scholer <rftpool23@rft2309.rz.fh-trier.de>
2012-07-24 13:50:04 +0200

    Top und Side Animation

--- projects/SotF/src/de/fhtrier/gdw2/sotf/AssetLoader.java
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/AssetLoader.java
@@ -11,6 +11,7 @@ import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.tiled.TiledMap;
 
 import de.fhtrier.gdw2.sotf.Interfaces.ITeam;
+
 public class AssetLoader {
 
 	ArrayList<Sound> sounds = new ArrayList<Sound>();
@@ -18,46 +19,80 @@ public class AssetLoader {
 	ArrayList<Animation> animations = new ArrayList<Animation>();
 	HashMap<Integer, String> maps = new HashMap<Integer, String>(10);
 	TiledMap map;
-	Animation playerAnimation;
+	Animation playerTop, playerSide;
 	SpriteSheet playerSheet;
-	
+
 	public AssetLoader() throws SlickException {
 		initializeAssets();
+
+	}
+
+	private void initializeAssets() throws SlickException {
+		// Maps anlegen
+		maps.put(0, "Testmap.tmx");
+		// Sheet für die Bilder der Animation anlegen + Bilder zur Animation
+		// hinzufügen
+		playerSheet = new SpriteSheet("res/animations/character_top.png", 130,
+				120);
+		playerTop = new Animation();
+		for (int col = 0; col < 15; ++col) {
+			playerTop.addFrame(playerSheet.getSprite(col, 0), 80);
+		}
+		
+		playerSide= new Animation();
+		playerSide.addFrame(new Image("res/animations/character_side.png"), 80);
 		
 	}
 	
-	private void initializeAssets() throws SlickException{
-		//Maps anlegen
-		maps.put(0, "Testmap.tmx");
-		playerSheet = new SpriteSheet("res/animations/character_top.png", 130, 120);
-		playerAnimation = new Animation();
-		playerAnimation.setAutoUpdate(true);
-		for(int col=0 ; col<15 ; ++col){
-			playerAnimation.addFrame(playerSheet.getSprite(col, 0), 80);
-		}
-		//Image sideView = new Image("res/animations/character_side.png");
-		//playerAnimation.addFrame(sideView, 300);
-		}
-	
-	public Sound getCollideSound(){
+	/**
+	 * 
+	 * Gibt einen Sound zurück der eine Kollision darstellt
+	 * @author Torsten Scholer
+	 * @param none
+	 */
+	public Sound getCollideSound() {
 		return null;
 	}
 	
-	public Sound getEatingSound(){
+	/**
+	 * 
+	 * Gibt einen Sound zurück der ein Essgeräusch darstellt
+	 * @author Torsten Scholer
+	 * @param none
+	 */
+	public Sound getEatingSound() {
 		return null;
 	}
+
 	/**
 	 * 
 	 * Lädt eine Map und gibt sie zurück.
+	 * 
 	 * @author <Torsten Scholer>
-	 * @param  stage - der index des levels
+	 * @param stage - der index des levels
 	 */
-	public TiledMap loadMap(int stage) throws SlickException{
-		map = new TiledMap("res/maps/" + maps.get(stage) , "res/maps/");
+	public TiledMap loadMap(int stage) throws SlickException {
+		map = new TiledMap("res/maps/" + maps.get(stage), "res/maps/");
 		return map;
 	}
+
+	/**
+	 * 
+	 * Gibt die Top-Animation eines Characters basierend auf seiner Teamzugehörigkeit zurück
+	 * @author Torsten Scholer
+	 * @param Iteam team - Das Team, dem der Spieler angehört
+	 */
+	public Animation getTopAnimation(ITeam team) {
+		return playerTop;
+	}
 	
-	public Animation getAnimation(ITeam team){
-		return playerAnimation;
+	/**
+	 * 
+	 * Gibt die Side-Animation eines Characters basierend auf seiner Teamzugehörigkeit zurück
+	 * @author Torsten Scholer
+	 * @param Iteam team - Das Team, dem der Spieler angehört
+	 */
+	public Animation getSideAnimation(ITeam team) {
+		return  playerSide;
 	}
 }
