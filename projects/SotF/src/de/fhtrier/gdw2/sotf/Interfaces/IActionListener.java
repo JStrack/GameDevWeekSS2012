aaf849dbbfbb60d7e87d01dadb44127148f06f49
Lusito <Lusito@10.143.110.19>
2012-07-24 11:05:43 +0200

    missing actionlistener interface

---
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/Interfaces/IActionListener.java
@@ -0,0 +1,13 @@
+package de.fhtrier.gdw2.sotf.Interfaces;
+
+/**
+ * Action listener
+ * 
+ * @author Lusito
+ */
+public interface IActionListener {
+	/**
+	 * Called on action
+	 */
+	void onAction();
+}
