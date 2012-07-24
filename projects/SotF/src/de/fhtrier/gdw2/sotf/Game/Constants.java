6d20e31b5d34cfc6ce2377b479cc2d90c3a0a460
JStrack <johannesstrack@live.de>
2012-07-24 14:19:29 +0200

    Umbenung des Asset Packages

--- projects/SotF/src/de/fhtrier/gdw2/sotf/Game/Constants.java
+++ projects/SotF/src/de/fhtrier/gdw2/sotf/Game/Constants.java
@@ -2,11 +2,14 @@ package de.fhtrier.gdw2.sotf.Game;
 
 /**
  * Enthält Balancing-Konstanten (um sie zentral zu lagern)
+ * 
  * @author Robin Dick
  */
-public class Constants {
-	/**
-	 * Anzahl der Pixel mit der ein Spieler in X- oder Y-Richtung in einer Sekunde bewegt werden kann
-	 */
-	public static float PLAYER_MOVEMENT_SPEED = 600f;
+public class Constants
+{
+    /**
+     * Anzahl der Pixel mit der ein Spieler in X- oder Y-Richtung in einer
+     * Sekunde bewegt werden kann
+     */
+    public static float PLAYER_MOVEMENT_SPEED = 600f;
 }
